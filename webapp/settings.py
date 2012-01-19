616fd8cea0d414fb98c1efdd7cf9e98670951488
Jeffrey Hu <golettoo@gmail.com>
2012-01-20 03:02:25 +0800

    remove local info

--- webapp/settings.py
+++ webapp/settings.py
@@ -204,8 +204,7 @@ FIXTURE_DIRS = (
     os.path.join(PROJECT_ROOT, 'fixtures'),
     )
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'golettoo@gmail.com'
-EMAIL_HOST_PASSWORD = 'huzhiwei'
+try:
+    from local_settings import *
+except ImportError:
+    pass
